f4327fd796aa83f763ede7d933147d04d24a5632
Mahshid Zeinaly <mahshid.zeinaly@gmail.com>
2014-06-12 12:28:53 -0700

    a program to find files with given name patterns

--- src/find_special_files.py
+++ src/find_special_files.py
@@ -2,12 +2,12 @@
 https://developers.google.com/edu/python/exercises/copy-special
 
 Write a program to print absolute path of filesnames 
+with pattern __something__
 '''
 
-
-import re
-import os
-import sys
+import re  # regular expression
+import os  # listdir path path.abspath
+import sys # argsv exit(1) stderr.write
 
 def find_special_files(dir):
   for f in os.listdir(dir):
@@ -16,9 +16,12 @@ def find_special_files(dir):
       file_path = os.path.join(dir, f)
       print os.path.abspath(file_path)
 
-
 def main():
-  dir = sys.argv[1]
+  args = sys.argv[1:]
+  if len(args) == 0:
+    print "must specify an input directory"
+    sys.exit(1)
+  dir = args[0]
   find_special_files(dir) 
 
 
